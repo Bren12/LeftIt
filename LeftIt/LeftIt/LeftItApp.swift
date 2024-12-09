7931657ef039480ef765b5bc24ebce8e330bd3b8
Brenda Saucedo <brenda.s.g12@hotmail.com>
2024-12-09 16:24:06 +0100

    Starting project

---
+++ LeftIt/LeftIt/LeftItApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftItApp.swift
+//  LeftIt
+//
+//  Created by Brenda Elena Saucedo Gonzalez on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LeftItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
