dbbe781690e8f871411d5c5e2d0847351b068831
Brenda Saucedo <brenda.s.g12@hotmail.com>
2024-12-09 17:44:57 +0100

    Starting new project

---
+++ LeafIt/LeafIt/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  LeafIt
+//
+//  Created by Brenda Elena Saucedo Gonzalez on 09/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ LeafIt/LeafIt/LeafItApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafItApp.swift
+//  LeafIt
+//
+//  Created by Brenda Elena Saucedo Gonzalez on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LeafItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
