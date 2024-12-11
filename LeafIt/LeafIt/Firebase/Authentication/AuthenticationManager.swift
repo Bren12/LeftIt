c476e074314e956d9c91884cb9c11db7b6b6d571
Brenda Saucedo <brenda.s.g12@hotmail.com>
2024-12-12 00:44:57 +0100

    sign in

---
+++ LeafIt/LeafIt/Firebase/Authentication/AuthenticationManager.swift
@@ -0,0 +1,147 @@
+//
+//  AuthenticationManager.swift
+//  LeafIt
+//
+//  Created by Brenda Elena Saucedo Gonzalez on 11/12/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoUrl: String?
+//    let isAnonymous: Bool
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+//        self.isAnonymous = user.isAnonymous
+    }
+}
+
+//enum AuthProviderOption: String {
+//    case email = "password"
+//    case google = "google.com"
+//    case apple = "apple.com"
+//}
+
+final class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    
+    private init() {}
+    
+    
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        } // -> guard
+        
+        return AuthDataResultModel(user: user)
+        
+    } // -> getAuthenticatedUser
+    
+    
+    
+    @discardableResult
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+        
+    } // -> createUser
+    
+    
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    } // -> signOut
+    
+    
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    } // -> resetPassword
+    
+    
+    
+    func updatePassword(password: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        try await user.updatePassword(to: password)
+    } // -> updatePassword
+    
+    
+    
+    func updateEmail(email: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        try await user.updateEmail(to: email)
+    } // -> updateEmail
+    
+    
+    
+//    func getProviders() throws -> [AuthProviderOption] {
+//        guard let providerData = Auth.auth().currentUser?.providerData else {
+//            throw URLError(.badServerResponse)
+//        }
+//        
+//        var providers: [AuthProviderOption] = []
+//        for provider in providerData {
+//            if let option = AuthProviderOption(rawValue: provider.providerID) {
+//                providers.append(option)
+//            } else {
+//                assertionFailure("Provider option not found: \(provider.providerID)")
+//            }
+//        }
+//        print(providers)
+//        return providers
+//    }
+       
+    
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+    } // -> signOut
+    
+    
+    
+//    func delete() async throws {
+//        guard let user = Auth.auth().currentUser else {
+//            throw URLError(.badURL)
+//        }
+//        
+//        try await user.delete()
+//    }
+    
+} // -> AuthenticationManager
+
+// MARK: SIGN IN ANONYMOUS
+
+//extension AuthenticationManager {
+//    
+//    @discardableResult
+//    func signInAnonymous() async throws -> AuthDataResultModel {
+//        let authDataResult = try await Auth.auth().signInAnonymously()
+//        return AuthDataResultModel(user: authDataResult.user)
+//    }
+//    
+//    private func linkCredential(credential: AuthCredential) async throws -> AuthDataResultModel {
+//        guard let user = Auth.auth().currentUser else {
+//            throw URLError(.badURL)
+//        }
+//        
+//        let authDataResult = try await user.link(with: credential)
+//        return AuthDataResultModel(user: authDataResult.user)
+//    }
+//    
+//    
+//}
